41a1b798cabea3e47b7ec401ec15360df1ee4f51
devnote-dev <devnote.dev75@gmail.com>
2023-05-01 04:51:22 +0100

    feat: add parse helper methods

--- src/strict-yaml.cr
+++ src/strict-yaml.cr
@@ -2,4 +2,14 @@ require "./strict-yaml/*"
 
 module StrictYAML
   VERSION = "0.1.0"
+
+  def self.parse(source : String) : Document
+    tokens = Lexer.new(source).run
+    Parser.new(tokens).parse_document
+  end
+
+  def self.parse_all(source : String) : Array(Document)
+    tokens = Lexer.new(source).run
+    Parser.new(tokens).parse_documents
+  end
 end
