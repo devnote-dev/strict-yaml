b7da22dee171a044d576b996602bbfaa39e2863a
devnote-dev <devnote.dev75@gmail.com>
2023-05-09 20:30:34 +0100

    fix(any): resolve casting issues

--- src/strict-yaml/any.cr
+++ src/strict-yaml/any.cr
@@ -1,6 +1,6 @@
 module StrictYAML
-  class Any
-    alias Type = String | Bool | Nil | Array(Type) | Hash(Type, Type)
+  struct Any
+    alias Type = String | Bool | Nil | Array(Any) | Hash(Any, Any)
 
     getter raw : Type
 
@@ -17,20 +17,12 @@ module StrictYAML
       @raw.as(Bool)
     end
 
-    def as_a : Array(Type)
+    def as_a : Array(Any)
       @raw.as(Array)
     end
 
-    def as_a(type : T.class) : Array(T) forall T
-      @raw.as(Array).map &.as(T)
-    end
-
-    def as_h : Hash(Type, Type)
+    def as_h : Hash(Any, Any)
       @raw.as(Hash)
-    end
-
-    def as_h(_k : K.class, _v : V.class) : Hash(K, V) forall K, V
-      @raw.as(Hash).map { |k, v| {k.as(K), v.as(V)} }.to_h
     end
   end
 end
