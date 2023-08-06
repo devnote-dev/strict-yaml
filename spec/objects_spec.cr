ccb4b6c374194aed2cd1c092c5dbbf85bfe09f3e
devnote-dev <devnote.dev75@gmail.com>
2023-08-06 21:38:53 +0100

    feat: implement object deserialization

---
+++ spec/objects_spec.cr
@@ -0,0 +1,140 @@
+require "./spec_helper"
+
+private enum Color
+  Red
+  Green
+  Blue
+end
+
+# @[Flags]
+# private enum Mode
+#   Read
+#   Write
+#   Async
+# end
+
+describe StrictYAML do
+  it "parses to integers" do
+    value = Int32.from_yaml "123456"
+
+    value.should be_a Int32
+    value.should eq 123456
+  end
+
+  it "parses to floats" do
+    value = Float64.from_yaml "3.14159265"
+
+    value.should be_a Float64
+    value.should eq 3.14159265_f64
+  end
+
+  it "parses to strings" do
+    value = String.from_yaml "foo bar baz"
+
+    value.should be_a String
+    value.should eq "foo bar baz"
+  end
+
+  it "parses to booleans" do
+    value = Bool.from_yaml "true"
+
+    value.should be_a Bool
+    value.should be_true
+
+    value = Bool.from_yaml "false"
+
+    value.should be_a Bool
+    value.should be_false
+  end
+
+  it "parses to char" do
+    value = Char.from_yaml "0"
+
+    value.should be_a Char
+    value.should eq '0'
+  end
+
+  it "parses to paths" do
+    value = Path.from_yaml "/dev/null"
+
+    value.should be_a Path
+    value.should eq Path["/dev/null"]
+  end
+
+  it "parses to an array" do
+    value = Array(Int32).from_yaml <<-YAML
+      - 123
+      - 456
+      - 789
+      YAML
+
+    value.should be_a Array(Int32)
+    value.size.should eq 3
+    value.should eq [123, 456, 789]
+  end
+
+  it "parses to a set" do
+    value = Set(Int32).from_yaml <<-YAML
+      - 456
+      - 123
+      - 789
+      - 123
+      - 456
+      YAML
+
+    value.should be_a Set(Int32)
+    value.size.should eq 3
+    value.should eq Set{456, 123, 789}
+  end
+
+  it "parses to a tuple" do
+    value = Tuple(String, String).from_yaml <<-YAML
+      - foo
+      - bar
+      YAML
+
+    value.should be_a Tuple(String, String)
+    value.should eq({"foo", "bar"})
+  end
+
+  it "parses to a hash" do
+    value = Hash(String, String).from_yaml <<-YAML
+      foo: bar
+      baz: qux
+      YAML
+
+    value.should be_a Hash(String, String)
+    value.should eq({"foo" => "bar", "baz" => "qux"})
+  end
+
+  it "parses to a named tuple" do
+    value = NamedTuple(foo: String, bar: Int32).from_yaml <<-YAML
+      foo: 12
+      bar: 34
+      YAML
+
+    value.should be_a NamedTuple(foo: String, bar: Int32)
+    value.should eq({foo: "12", bar: 34})
+  end
+
+  it "parses normal enums" do
+    value = Array(Color).from_yaml <<-YAML
+        - red
+        - blue
+        YAML
+
+    value.should be_a Array(Color)
+    value.should eq [Color::Red, Color::Blue]
+  end
+
+  # FIXME: doesn't work in specs because of macro generation
+  # it "parses flag enums" do
+  #   value = Mode.from_yaml <<-YAML
+  #       - read
+  #       - write
+  #       YAML
+
+  #   value.should be_a Mode
+  #   value.should eq Mode::Read | Mode::Write
+  # end
+end
---
+++ src/strict-yaml/objects.cr
@@ -0,0 +1,201 @@
+class Object
+  def self.from_yaml(source : String)
+    new StrictYAML.parse source
+  end
+
+  def self.from_yaml(value : StrictYAML::Any)
+    new value
+  end
+end
+
+struct Nil
+  def self.new(value : StrictYAML::Any)
+    nil
+  end
+end
+
+{% for base in %w[8 16 32 64 128] %}
+  struct Int{{ base.id }}
+    def self.new(value : StrictYAML::Any)
+      value.to_i{{ base.id }}
+    end
+  end
+{% end %}
+
+{% for base in %w[32 64] %}
+  struct Float{{ base.id }}
+    def self.new(value : StrictYAML::Any)
+      value.to_f{{ base.id }}
+    end
+  end
+{% end %}
+
+class String
+  def self.new(value : StrictYAML::Any)
+    value.as_s
+  end
+end
+
+struct Bool
+  def self.new(value : StrictYAML::Any)
+    value.as_bool
+  end
+end
+
+struct Char
+  def self.new(value : StrictYAML::Any)
+    chars = value.as_s.chars
+    raise "invalid character sequence" unless chars.size == 1
+    chars[0]
+  end
+end
+
+struct Path
+  def self.new(value : StrictYAML::Any)
+    new value.as_s
+  end
+end
+
+class Array(T)
+  def self.new(value : StrictYAML::Any)
+    arr = new
+    value.as_a.each do |child|
+      arr << T.new child
+    end
+    arr
+  end
+end
+
+class Deque(T)
+  def self.new(value : StrictYAML::Any)
+    deq = new
+    value.as_a.each do |child|
+      deq << T.new child
+    end
+    deq
+  end
+end
+
+struct Set(T)
+  def self.new(value : StrictYAML::Any)
+    set = new
+    value.as_a.each do |child|
+      set << T.new child
+    end
+    set
+  end
+end
+
+struct Tuple(*T)
+  def self.new(value : StrictYAML::Any)
+    {% begin %}
+      arr = value.as_a
+      new(
+        {% for i in 0...T.size %}
+          (self[{{ i }}].new arr[{{ i }}]),
+        {% end %}
+      )
+    {% end %}
+  end
+end
+
+class Hash(K, V)
+  def self.new(value : StrictYAML::Any)
+    hash = new
+    value.as_h.each do |k, v|
+      hash[K.new(k)] = V.new(v)
+    end
+    hash
+  end
+end
+
+struct NamedTuple
+  def self.new(value : StrictYAML::Any)
+    {% begin %}
+      {% for key, type in T %}
+        {% if type.nilable? %}
+          %var{key.id} = nil
+        {% else %}
+          %var{key.id} = uninitialized {{ type }}
+        {% end %}
+        %found{key.id} = false
+      {% end %}
+
+      value.as_h.each do |k, v|
+        case k.as_s
+        {% for key, type in T %}
+          when {{ key.stringify }}
+            %var{key.id} = {{ type }}.from_yaml v
+            %found{key.id} = true
+        {% end %}
+        end
+      end
+
+      {% for key, type in T %}
+        {% unless type.nilable? %}
+          unless %found{key.id}
+            raise "missing YAML attribute: {{ key.id }}"
+          end
+        {% end %}
+      {% end %}
+
+      new(
+        {% for key in T.keys %}
+          {{ key.id.stringify }}: %var{key.id},
+        {% end %}
+      )
+    {% end %}
+  end
+end
+
+struct Enum
+  def self.new(value : StrictYAML::Any)
+    {% if @type.annotation(Flags) %}
+      case value.raw
+      when String
+        parse?(value.as_s) || raise "unknown enum #{self} value: #{value.as_s.inspect}"
+      when Array
+        data = {{ @type }}::None
+        value.as_a.each do |item|
+          data |= parse?(item.as_s) || raise "unknown enum #{self} value: #{item.as_s.inspect}"
+        end
+        data
+      else
+        raise "cannot parse enum from type #{value.raw.class}"
+      end
+    {% else %}
+      parse?(value.as_s) || raise "unknown enum #{self} value: #{value.as_s.inspect}"
+    {% end %}
+  end
+end
+
+struct Union(*T)
+  def self.new(value : StrictYAML::Any)
+    {% begin %}
+      {% for type in T %}
+        {% if Number::Primitive.union_types.includes?(type) %}
+          return {{ type }}.new value
+        {% elsif type == String %}
+          return value.as_s
+        {% elsif type == Bool %}
+          return value.as_bool
+        {% end %}
+      {% end %}
+
+      {% primitives = [Nil, String, Bool] + Number::Primitive.union_types %}
+      {% others = T.reject { |t| primitives.includes?(t) } %}
+      {% for type in others %}
+        begin
+          return {{ type }}.from_yaml value
+        rescue StrictYAML::ParseError
+        end
+      {% end %}
+
+      {% if T.includes?(Nil) %}
+        return nil
+      {% else %}
+        raise StrictYAML::ParseError.new "could not parse #{self} from '#{value}'"
+      {% end %}
+    {% end %}
+  end
+end
