0be7c6737c69829731df13dc698334725dd5dd40
devnote-dev <devnote.dev75@gmail.com>
2023-08-07 04:28:14 +0100

    refactor(builder): rework builder logic

--- src/strict-yaml/builder.cr
+++ src/strict-yaml/builder.cr
@@ -1,166 +1,111 @@
 module StrictYAML
   class Builder
-    alias Type = String | Bool | Nil | Array(Type) | Hash(Type, Type)
+    @[Flags]
+    enum State
+      ListStart
+      ListBlock
+    end
 
-    @nodes : Array(Node)
     @io : IO
+    @level : Int32
     @indent : Int32
     @newline : Bool
+    @state : State
     getter? closed : Bool
 
     def initialize(@io : IO)
-      @nodes = [] of Node
+      @level = 0
       @indent = 0
-      @newline = true
+      @newline = false
+      @state = :none
       @closed = false
     end
 
     def document(*, version : String? = nil, & : ->) : Nil
       document_start version: version
       yield
+      check_state
       document_end
     end
 
     def document_start(*, version : String? = nil) : Nil
       unless version.nil?
-        @nodes << Directive.new "YAML #{version}"
+        @io << "%YAML " << version << '\n'
       end
-      @nodes << DocumentStart.empty
+      @io << "---"
+      @newline = true
     end
 
     def document_end : Nil
-      @nodes << DocumentEnd.empty
+      @io << "..."
+      @newline = true
     end
 
-    def scalar(value : Type) : Nil
-      case value
-      when String
-        @nodes << Scalar.new value
-      when Bool
-        @nodes << Boolean.new value
-      when Nil
-        @nodes << Null.empty
+    def scalar(value : _, *, quote : Bool = false) : Nil
+      check_state
+      if quote
+        @io << '"' << value << '"'
       else
-        @nodes << Scalar.new value.to_s
+        @io << value
       end
+      @newline = true
     end
 
-    def mapping(key : Type, value : Type) : Nil
-      @nodes << Map.from [Mapping.new(parse(key), parse(value))]
+    def boolean(value : Bool) : Nil
+      check_state
+      @io << value
+      @newline = true
     end
 
-    def mapping(key : Type, & : ->) : Nil
-      original = @nodes.dup
-      @nodes.clear
-      yield
-      values = @nodes
-      @nodes = original
-      value = values.size > 1 ? List.new(values) : values[0]
-
-      @nodes << Map.from [Mapping.new(parse(key), value)]
+    def null : Nil
+      check_state
+      @io << "null"
+      @newline = true
     end
 
     def list(& : ->) : Nil
-      original = @nodes.dup
-      @nodes.clear
+      check_state
+      @level += 1
+      @indent += 2
+      @state |= State::ListStart
+
       yield
 
-      values = @nodes
-      @nodes = original
-      @nodes << List.new values
+      @level -= 1
+      @indent -= 2
+      @state &= ~State::ListBlock
+    end
+
+    def mapping : Nil
     end
 
     def comment(text : String) : Nil
-      @nodes << Comment.new text
+      @io << "  # " << text
+      @newline = true
     end
 
     def close : Nil
       return if @closed
 
-      @nodes.each_with_index do |node, index|
-        @io << (" " * @indent) unless @indent.zero?
-        visit node
-        @io << '\n' if @newline
-      end
-
-      @closed = true
+      check_state
       @io.flush
+      @closed = true
     end
 
-    private def parse(value : Type) : Node
-      case value
-      in String
-        Scalar.new value
-      in Bool
-        Boolean.new value
-      in Nil
-        Null.empty
-      in Hash
-        Map.from value.map { |k, v| Mapping.new parse(k), parse(v) }
-      in Array
-        List.new value.map { |v| parse(v) }
-      end
-    end
-
-    private def visit(node : DocumentStart) : Nil
-      @io << "---"
-    end
-
-    private def visit(node : DocumentEnd) : Nil
-      @io << "..."
-    end
-
-    private def visit(node : Scalar) : Nil
-      @io << node.value
-    end
-
-    private def visit(node : Boolean) : Nil
-      @io << node.value
-    end
-
-    private def visit(node : Null) : Nil
-      @io << "null"
-    end
-
-    private def visit(node : Map) : Nil
-      last = node.entries.size - 1
-      node.entries.each_with_index do |(key, value), index|
-        @io << (" " * @indent) unless @indent.zero?
-        visit key
-        @io << ':'
-
-        if value.is_a?(Map | List)
-          @io << '\n'
-          @indent += 2
-          visit value
-          @indent -= 2
-        else
-          @io << ' '
-          visit value
-        end
-
-        @io << '\n' unless index == last
-      end
-    end
-
-    private def visit(node : List) : Nil
-      node.values.each do |value|
-        @io << (" " * @indent) unless @indent.zero?
-        @io << "- "
-        visit value
+    private def check_state : Nil
+      if @newline
         @io << '\n'
+        @newline = false
       end
-    end
 
-    private def visit(node : Comment) : Nil
-      @io << "  # " << node.value
-    end
-
-    private def visit(node : Directive) : Nil
-      @io << '%' << node.value
-    end
-
-    private def visit(node : Node) : Nil
+      if @level > 0
+        @io << (" " * @indent) if @level > 2 && @state.list_block?
+        if @state.list_start?
+          @state &= ~State::ListStart
+          @state |= State::ListBlock
+        end
+        @io << "- " * @level && @state.list_block?
+      end
     end
   end
 end
