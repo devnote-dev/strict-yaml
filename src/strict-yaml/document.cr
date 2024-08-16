6ba09ba776f3e8edc6e3172d007a68933799c465
devnote-dev <devnote.dev75@gmail.com>
2024-08-16 16:23:12 +0100

    feat(document): implement any conversion for single node & mapping

--- src/strict-yaml/document.cr
+++ src/strict-yaml/document.cr
@@ -21,5 +21,41 @@ module StrictYAML
 
     def version=(@version : String)
     end
+
+    def to_any : Any
+      values = @nodes.select do |node|
+        case node
+        when Comment, Directive, DocumentStart, DocumentEnd
+          false
+        else
+          true
+        end
+      end
+      root = values[0]
+
+      values.each do |node|
+        unless node.class == root.class
+          raise Error.new "#{node.class} value is not allowed in this context", node.loc
+        end
+      end
+
+      case root
+      when Mapping
+        hash = values.each_with_object({} of Any => Any) do |n, h|
+          h.merge! n.to_object.as(Hash(Any, Any))
+        end
+
+        Any.new hash
+      when List
+        # arr = nodes.map(&.to_object).reduce([] of Any) do |acc, i|
+        #   acc + i.as(Array(Any))
+        # end
+
+        # Any.new arr
+        raise "not implemented"
+      else
+        Any.new values[0].to_object
+      end
+    end
   end
 end
