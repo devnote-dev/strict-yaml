b97bb5750218393ac6243fe22f2cc484ad8f0c65
devnote-dev <devnote.dev75@gmail.com>
2024-08-16 16:28:45 +0100

    refactor: replace syntax tree parsing with document/stream

--- src/strict-yaml.cr
+++ src/strict-yaml.cr
@@ -4,39 +4,19 @@ module StrictYAML
   VERSION = "0.1.0"
 
   def self.parse(source : String) : Any
-    parse_all(source).first
+    parse_document(source).to_any
   end
 
   def self.parse_all(source : String) : Array(Any)
-    parse_documents(source).map(&.nodes).map do |nodes|
-      case nodes[0]
-      when List
-        arr = nodes.map(&.to_object).reduce([] of Any) do |acc, i|
-          acc + i.as(Array(Any))
-        end
-
-        Any.new arr
-      when Mapping
-        hash = nodes.map(&.to_object).reduce({} of Any => Any) do |acc, i|
-          acc.merge i.as(Hash(Any, Any))
-        end
-
-        Any.new hash
-      else
-        Any.new nodes[0].to_object
-      end
-    end
+    parse_documents(source).map(&.to_any)
   end
 
   def self.parse_document(source : String) : Document
-    parse_documents(source).first
+    parse_documents(source)[0]
   end
 
   def self.parse_documents(source : String) : Array(Document)
     tokens = Lexer.run source
-    ast = Parser.parse tokens
-    ast.raise if ast.issues?
-
-    ast.parse_documents
+    Parser.parse(tokens).documents
   end
 end
