38bb13aaf3bacb7ca201bf8933065b8e98370a26
devnote-dev <devnote.dev75@gmail.com>
2024-10-04 10:22:37 +0100

    feat: include additional parser methods in module helpers

--- src/strict-yaml.cr
+++ src/strict-yaml.cr
@@ -11,12 +11,19 @@ module StrictYAML
     parse_documents(source).map(&.to_any)
   end
 
-  def self.parse_document(source : String, preserve : Bool = false) : Document
-    parse_documents(source, preserve)[0]
+  def self.parse_document(source : String, *, preserve : Bool = false,
+                          parse_scalars : Bool = true, sensitive : Bool = false) : Document
+    parse_documents(source, preserve: preserve, sensitive: sensitive)[0]
   end
 
-  def self.parse_documents(source : String, preserve : Bool = false) : Array(Document)
+  def self.parse_documents(source : String, *, preserve : Bool = false,
+                           parse_scalars : Bool = true, sensitive : Bool = false) : Array(Document)
     tokens = Lexer.run source
-    Parser.parse(tokens, preserve: preserve).documents
+    Parser.parse(
+      tokens,
+      preserve: preserve,
+      parse_scalars: parse_scalars,
+      sensitive_scalars: sensitive
+    ).documents
   end
 end
